8790d60e32aff91f0300ad66e55d3520c581539e
RouterPlus Networks <weibo@routerplus.com>
2021-12-16 13:25:49 +0800

    Add files via upload

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,14 @@
 const net = require('net')
 
-var factory = require('../build/libssh2.js');
+var factory = require('../dist/libssh2.js');
 var SESSION;
 
 factory().then((libssh2) => {
-
 	SESSION = libssh2.SESSION;
+	CHANNEL = libssh2.CHANNEL;
+	SFTP    = libssh2.SFTP;
+
+	libssh2.init(0);
 
 	test();
 });
@@ -16,26 +19,23 @@ function test()
 	let client = new SESSION(socket);
 
 	client.send = function(buffer, length) {
-		// console.log('buffer', buffer);
-		console.log('to raw', buffer.length)
+		//console.log('to raw', buffer.length)
 		socket.write(buffer);
 	}
 
 	socket.connect(22, '127.0.0.1', function() {
-		socket.on('error', (err)=> {
-		
+		socket.on('error', (err)=> {	
 		});
 
 		socket.on('data', (msg) => {
-			console.log('to ssh', msg, msg.length);
+			//console.log('to ssh2', msg.length);
 			client.pushdata(msg);
 
 		});
 
 		socket.on('close', () => {
-
+			console.log('socket closed');
+			delete client
 		});
         });
-
-	//delete client
 }
