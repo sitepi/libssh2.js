3dea2a299abfb7dcb85afd48ddcc00080d1986ae
RouterPlus Networks <weibo@routerplus.com>
2021-12-19 11:19:13 +0800

    Add files via upload

--- tests/test.js
+++ tests/test.js
@@ -1,48 +1,33 @@
 const net = require('net')
 
-
 var factory = require('../dist/libssh2.js');
-var SESSION;
+var libssh2;
 
-factory().then((libssh2) => {
-	SESSION = libssh2.SESSION;
-	//CHANNEL = libssh2.CHANNEL;
-	//SFTP    = libssh2.SFTP;
-
+factory().then((wasm) => {
+	libssh2 = wasm;
 	libssh2.init(0);
-
 	test();
 });
 
-async function test() 
+let user  = 'user'
+let passwd= 'xxxxxx'
+
+function test() 
 {
+	let client;
 	let socket = new net.Socket();
-	//let client = new SESSION(socket);
 
-//	client.send = function(buffer, length) {
-//		//console.log('to raw', buffer.length)
-//		socket.write(buffer);
-//	}
-
-	socket.connect(22, '127.0.0.1', function() {
-		socket.on('error', (err)=> {
-			console.error('socket error', err);
+	client = libssh2.createSESSION(socket, ()=> {
+		
+		client.login(user, passwd, (rc) => {
+			if(rc !== libssh2.ERROR.NONE) {
+				console.log(rc, libssh2.ERRMSG[rc]);
+			}
+			else {
+				has_logined = true;
+				console.log(user,'logined');
+			}
 		});
-
-		socket.on('data', (msg) => {
-			//console.log('to ssh2', msg.length);
-			client.pushdata(msg);
-		});
-
-		socket.on('close', () => {
-			console.error('socket closed');
-			delete client
-		});
-		client = new SESSION(socket)
-
-		client.send = function(buffer, length) {
-                        socket.write(buffer);
-                }
-
-        });
+	});
+	socket.connect(22, '127.0.0.1');
 }
